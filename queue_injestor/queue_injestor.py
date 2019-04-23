63149a5063dbbff6137f56ba3ac3082f576fceb3
Matthew Amos <mamos@scottlogic.com>
2019-04-23 12:29:43 +0100

    oops tests in code

--- queue_injestor/queue_injestor.py
+++ queue_injestor/queue_injestor.py
@@ -1,14 +1,1 @@
-import pytest
-
-
-# TODO real tests
-
-
-@pytest.mark.unit
-def test_nothing_much_yet():
-    pass
-
-
-@pytest.mark.integration
-def test_nothing_much_yet_either():
-    pass
+# TODO
