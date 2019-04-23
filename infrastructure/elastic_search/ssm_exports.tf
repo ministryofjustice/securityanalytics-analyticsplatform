5658ac65d16a1a412b7dd56801627cfdd6c4bbcd
Matthew Amos <35695811+blackmamo@users.noreply.github.com>
2019-04-23 17:34:13 +0100

    Analytics needs to publish arn and id (#8)
    * Analytics needs to publish arn and id

--- infrastructure/elastic_search/ssm_exports.tf
+++ infrastructure/elastic_search/ssm_exports.tf
@@ -1,8 +1,21 @@
-resource "aws_ssm_parameter" "elastic_injestion_queue" {
+resource "aws_ssm_parameter" "elastic_injestion_queue_arn" {
   name        = "/${var.app_name}/${terraform.workspace}/analytics/elastic/injest_queue/arn"
   description = "The arn of the injestion queue"
   type        = "String"
   value       = "${aws_sqs_queue.injestion-queue.arn}"
+  overwrite   = "true"
+
+  tags {
+    app_name  = "${var.app_name}"
+    workspace = "${terraform.workspace}"
+  }
+}
+
+resource "aws_ssm_parameter" "elastic_injestion_queue_id" {
+  name        = "/${var.app_name}/${terraform.workspace}/analytics/elastic/injest_queue/id"
+  description = "The id (url) of the injestion queue"
+  type        = "String"
+  value       = "${aws_sqs_queue.injestion-queue.id}"
   overwrite   = "true"
 
   tags {
