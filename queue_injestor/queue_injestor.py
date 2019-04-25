f8bac9aac3702021330146fb042715fe705b93df
Phil Rogers <progers@scottlogic.com>
2019-04-25 16:26:55 +0100

    WIP

--- queue_injestor/queue_injestor.py
+++ queue_injestor/queue_injestor.py
@@ -24,33 +24,26 @@ ssm_prefix = f"/{app_name}/{stage}"
 ssm_client = boto3.client("ssm", region_name=region)
 sqs_client = boto3.client("sqs", region_name=region)
 es_url = 'https://search-d-progers-sec-an-es-llytvwd7nqmfijkmjgvfx35lpu.eu-west-2.es.amazonaws.com/'+'sec-an'+'/data'
+SSM_ES_ENDPOINT = f"{ssm_prefix}/analytics/elastic/es_endpoint/arn"
 
 
-# @ssm_parameters(
-#     ssm_client
-# )
-def injest(event, _):
+@ssm_parameters(
+    ssm_client,
+    SSM_ES_ENDPOINT
+)
+@async_handler
+async def injest(event, _):
     print(f'region {region}')
-    SUBNETS = f"{ssm_prefix}/vpc/subnets/instance"
-    es_url = f"{ssm_prefix}/analytics/elastic/es_endpoint/arn"
-    print(f'{SUBNETS}')
+    print(event)
+    ssm_params = event["ssm_params"]
+    print(ssm_params)
+    es_url = ssm_params[SSM_ES_ENDPOINT]
+
+    print(f'ES URL:{es_url}')
     for event in event["Records"]:
 
         print(f"new record: {dumps(event['body'])}")
-        # TODO: get the URL of elasticesearch and construct an endpoint
-        # es_url = f"{var.elastic_search}"
-
         print(es_url)
-        # send the record from queue to ES
-        # r = requests.post(es_url, auth=awsauth,
-        #                   json=event['body'], headers=headers)
         headers = {'content-type': 'application/json'}
         r = requests.post(es_url, dumps(event['body']), headers=headers)
         print(f"post completed, return {r.text}")
-    # print(event)
-    # print('hello')
-    # print(sqs_client)
-
-
-# take from the queue - see results_parser.py
-# push to ES
