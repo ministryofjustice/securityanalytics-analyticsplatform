bbb207ac7f5bffa6a78d0f462551878fc77267e2
Matthew Amos <35695811+blackmamo@users.noreply.github.com>
2019-05-07 16:43:00 +0100

    Feature/sa 48 remove serverless (#12)
    * Remove serverless

---
+++ infrastructure/elastic_search/queue_ingestor.tf
@@ -0,0 +1,52 @@
+locals {
+  analytics_zip = "../.generated/sec-an-analytics.zip"
+}
+
+data "external" "analytics_zip" {
+  program = [
+    "python",
+    "../shared_code/python/package_lambda.py",
+    "${local.analytics_zip}",
+    "${path.module}/packaging.config.json",
+    "../Pipfile.lock",
+  ]
+}
+
+resource "aws_lambda_permission" "sqs_invoke" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.queue_ingestor.function_name}"
+  principal     = "sqs.amazonaws.com"
+  source_arn    = "${aws_sqs_queue.ingestion_queue.arn}"
+}
+
+resource "aws_lambda_event_source_mapping" "ingestor_queue_trigger" {
+  depends_on       = ["aws_lambda_permission.sqs_invoke", "aws_iam_role_policy_attachment.queue_ingestor"]
+  event_source_arn = "${aws_sqs_queue.ingestion_queue.arn}"
+  function_name    = "${aws_lambda_function.queue_ingestor.arn}"
+  enabled          = true
+  batch_size       = 1
+}
+
+resource "aws_lambda_function" "queue_ingestor" {
+  depends_on = ["data.external.analytics_zip"]
+
+  function_name = "${terraform.workspace}-${var.app_name}-analytics-ingestor"
+  handler       = "queue_ingestor.queue_ingestor.ingest"
+  role          = "${aws_iam_role.queue_ingestor.arn}"
+  runtime       = "python3.7"
+  filename      = "${local.analytics_zip}"
+
+  layers = [
+    "${data.aws_ssm_parameter.utils_layer.value}",
+  ]
+
+  environment {
+    variables = {
+      REGION    = "${var.aws_region}"
+      STAGE     = "${terraform.workspace}"
+      APP_NAME  = "${var.app_name}"
+      TASK_NAME = "analytics"
+    }
+  }
+}
