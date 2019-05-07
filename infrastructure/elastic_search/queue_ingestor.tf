041fd3e42e3f664d021fb36bd39034121bccf9a6
Matthew Amos <mamos@scottlogic.com>
2019-05-07 16:11:15 +0100

    Fixed tags

--- infrastructure/elastic_search/queue_ingestor.tf
+++ infrastructure/elastic_search/queue_ingestor.tf
@@ -45,7 +45,7 @@ resource "aws_lambda_function" "queue_ingestor" {
     variables = {
       REGION    = "${var.aws_region}"
       STAGE     = "${terraform.workspace}"
-      APP_NAME  = "sec-an"
+      APP_NAME  = "${var.app_name}"
       TASK_NAME = "analytics"
     }
   }
