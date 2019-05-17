daaf6dd92c9a3c6f11636dff1f1bd38e6e87c5e4
Matthew Amos <mamos@scottlogic.com>
2019-05-17 14:47:01 +0100

    Cleaner

--- infrastructure/elastic_index/outputs.tf
+++ infrastructure/elastic_index/outputs.tf
@@ -1,7 +1,7 @@
 output "history_index_id" {
-  value = "${null_resource.setup_new_index.*.triggers.index_hash[0]}"
+  value = "${null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "history")]}"
 }
 
 output "snapshot_index_id" {
-  value = "${null_resource.setup_new_index.*.triggers.index_hash[1]}"
+  value = "${null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "snapshot")]}"
 }
