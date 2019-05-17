82f0835c2f580a4576a0a4d08b915cf047ead999
Matthew Amos <mamos@scottlogic.com>
2019-05-17 11:05:52 +0100

    Add in support for two sets of indexes per stream, for the times series and latest

--- queue_ingestor/queue_ingestor.py
+++ queue_ingestor/queue_ingestor.py
@@ -6,6 +6,7 @@ from utils.lambda_decorators import ssm_parameters
 from utils.json_serialisation import dumps
 import requests
 from json import loads
+import warnings
 
 region = os.environ["REGION"]
 stage = os.environ["STAGE"]
@@ -15,6 +16,7 @@ ssm_prefix = f"/{app_name}/{stage}"
 ssm_client = boto3.client("ssm", region_name=region)
 sqs_client = boto3.client("sqs", region_name=region)
 SSM_ES_ENDPOINT = f"{ssm_prefix}/analytics/elastic/es_endpoint/url"
+HEADERS = {"content-type": "application/json"}
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, "es", session_token=credentials.token)
 
@@ -26,18 +28,35 @@ awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, "es",
 @async_handler
 async def ingest(event, _):
     print(f"Processing {event}")
-    ssm_params = event["ssm_params"]
+    endpoint = event["ssm_params"][SSM_ES_ENDPOINT]
 
     for event in event["Records"]:
         body = loads(event["body"])
         subject = body["Subject"]
-        es_url = f"https://{ssm_params[SSM_ES_ENDPOINT]}/{subject}/_doc"
         message = body["Message"]
         print(f"Posting {message} to {es_url}")
-        headers = {"content-type": "application/json"}
 
-        r = requests.post(es_url, auth=awsauth, data=message, headers=headers)
-        print(f"Post completed {r.text}")
-        response_json = r.json()
-        if "error" in response_json.keys():
-            raise RuntimeError(dumps(response_json["error"]))
+        # TODO (https://dsdmoj.atlassian.net/browse/SA-91)
+        if "MessageAttributes" not in body:
+            warnings.warn("No message attributes found in results message, falling back to old behaviour")
+            post_to_es(endpoint, subject, message)
+        else:
+            attrs = body["MessageAttributes"]
+            scan_id = attrs["ScanId"]
+            non_temporal_key = attrs["NonTemporalKey"]
+            # This post is the history, used in time series
+            # Note that the use of scan_id enables re-ingestion
+            post_to_es(endpoint, subject, message, scan_id)
+            # This post is going to update the latest doc for this non temporal key
+            # i.e. this produces an index where we can access the latest version of each scan.
+            post_to_es(endpoint, f"{subject}_latest", message, non_temporal_key)
+
+
+def post_to_es(endpoint, subject, message, doc_id=None):
+    doc_id = f"/{doc_id}" if doc_id else ""
+    es_url = f"https://{endpoint}/{subject}/_doc{doc_id}"
+    r = requests.post(es_url, auth=awsauth, data=message, headers=HEADERS)
+    print(f"Post completed {r.text}")
+    response_json = r.json()
+    if "error" in response_json.keys():
+        raise RuntimeError(dumps(response_json["error"]))
