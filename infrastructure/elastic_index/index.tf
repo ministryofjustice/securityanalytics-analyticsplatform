d4afa8875944f4432b957ec2f922a50e31f9a840
Matthew Amos <35695811+blackmamo@users.noreply.github.com>
2019-05-20 13:15:57 +0100

    Feature/sa 79 add latest and series (#20)
    * Add in support for two sets of indexes per stream, for the times series and latest

--- infrastructure/elastic_index/index.tf
+++ infrastructure/elastic_index/index.tf
@@ -3,34 +3,37 @@ data "local_file" "index_definition" {
 }
 
 data "external" "current_index" {
+  count = "${length(local.flavours)}"
+
   program = [
     "python",
     "${path.module}/get-current-write-index.py",
     "${var.aws_region}",
     "${var.app_name}",
     "${var.task_name}",
-    "${var.index_name}",
+    "${var.index_name}_${local.flavours[count.index]}",
     "${data.aws_ssm_parameter.es_domain.value}",
   ]
 }
 
 locals {
-  index_hash     = "${md5(data.local_file.index_definition.content)}"
-  script_hash    = "${md5(file("${path.module}/write-new-index.py"))}"
-  old_index_hash = "${data.external.current_index.result.index}"
+  flavours = ["history", "snapshot"]
 }
 
 resource "null_resource" "setup_new_index" {
   # This count stops us from re-indexing dev, when looking at integration tests
-  count = "${var.ssm_source_stage == terraform.workspace ? 1 : 0}"
+  count = "${var.ssm_source_stage == terraform.workspace ? length(local.flavours) : 0}"
 
   triggers {
-    index_hash  = "${local.index_hash}"
-    script_hash = "${local.script_hash}"
+    index_hash  = "${md5(data.local_file.index_definition.content)}"
+    script_hash = "${md5(file("${path.module}/write-new-index.py"))}"
+
+    # Since terraform has no way to query the actual state of these resources, it will not re-create them if they have been deleted. This (although making the build noisy), will ensure that they are always created.
+    allways = "${timestamp()}"
   }
 
   provisioner "local-exec" {
     # Doesn't just write the new one, it also updates the aliases and starts re-indexing
-    command = "python ${path.module}/write-new-index.py ${var.aws_region} ${var.app_name} ${var.task_name} ${var.index_name} ${local.index_hash} ${data.local_file.index_definition.filename} ${data.aws_ssm_parameter.es_domain.value} ${local.old_index_hash}"
+    command = "python ${path.module}/write-new-index.py ${var.aws_region} ${var.app_name} ${var.task_name} ${var.index_name}_${local.flavours[count.index]} ${self.triggers.index_hash} ${data.local_file.index_definition.filename} ${data.aws_ssm_parameter.es_domain.value} ${data.external.current_index.*.result.index[count.index]}"
   }
 }
--- infrastructure/elastic_index/outputs.tf
+++ infrastructure/elastic_index/outputs.tf
@@ -1,3 +1,7 @@
-output "index_id" {
-  value = "${local.index_hash}"
+output "history_index_id" {
+  value = "${null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "history")]}"
+}
+
+output "snapshot_index_id" {
+  value = "${null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "snapshot")]}"
 }
