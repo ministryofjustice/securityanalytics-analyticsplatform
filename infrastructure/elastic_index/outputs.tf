7ea89ed617a2c85c69bea60e08d7d60b77adf796
Matthew Amos <mamos@scottlogic.com>
2019-05-21 09:05:11 +0100

    Can't reference the output when the count is 0

--- infrastructure/elastic_index/outputs.tf
+++ infrastructure/elastic_index/outputs.tf
@@ -1,7 +1,8 @@
 output "history_index_id" {
-  value = "${null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "history")]}"
+  value = "${var.ssm_source_stage == terraform.workspace ?
+  null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "history")] : "NoIndex"}"
 }
 
 output "snapshot_index_id" {
-  value = "${null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "snapshot")]}"
+  value = "${var.ssm_source_stage == terraform.workspace ? null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "snapshot")]: "NoIndex"}"
 }
