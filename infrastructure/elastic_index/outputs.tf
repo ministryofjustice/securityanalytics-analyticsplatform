0b447c6d1df491a5b19967310af68acadd46f45a
Matthew Amos <mamos@scottlogic.com>
2019-05-21 09:23:51 +0100

    diagnose

--- infrastructure/elastic_index/outputs.tf
+++ infrastructure/elastic_index/outputs.tf
@@ -1,8 +1,8 @@
 output "history_index_id" {
   value = "${var.ssm_source_stage == terraform.workspace ?
-  null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "history")] : "NoIndex"}"
+  null_resource.setup_new_index.*.triggers.index_hash[0] : "NoIndex"}"
 }
 
 output "snapshot_index_id" {
-  value = "${var.ssm_source_stage == terraform.workspace ? null_resource.setup_new_index.*.triggers.index_hash[index(local.flavours, "snapshot")]: "NoIndex"}"
+  value = "${var.ssm_source_stage == terraform.workspace ? null_resource.setup_new_index.*.triggers.index_hash[1]: "NoIndex"}"
 }
