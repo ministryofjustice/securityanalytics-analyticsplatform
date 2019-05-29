26a2af82f4b433d9e037f15a13674e2994e5b408
Phil Rogers <47106782+progers-SL@users.noreply.github.com>
2019-05-29 17:05:39 +0100

    sns/sqs policy moved here

--- infrastructure/elastic_search/ingestion_queue.tf
+++ infrastructure/elastic_search/ingestion_queue.tf
@@ -1,3 +1,31 @@
+data "aws_iam_policy_document" "notify_topic_policy" {
+  statement {
+    actions = [
+      "sqs:SendMessage",
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+
+      values = [
+        "arn:aws:sns:${var.aws_region}:${var.account_id}:*",
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["sns.amazonaws.com"]
+    }
+
+    resources = [
+      "${aws_sqs_queue.ingestion_queue.arn}",
+    ]
+  }
+}
+
 resource "aws_sqs_queue" "ingestion_queue" {
   name = "${terraform.workspace}-${var.app_name}-es-ingestion-queue"
 
@@ -5,4 +33,9 @@ resource "aws_sqs_queue" "ingestion_queue" {
     app_name  = "${var.app_name}"
     workspace = "${terraform.workspace}"
   }
+  
+
+resource "aws_sqs_queue_policy" "queue_policy" {
+  queue_url = "${aws_sqs_queue.ingestion_queue.id}"
+  policy    = "${data.aws_iam_policy_document.notify_topic_policy.json}"
 }
