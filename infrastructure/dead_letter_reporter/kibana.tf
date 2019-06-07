eed2aa0447af4ed1e002712104415a6d9c307f85
Matthew Amos <mamos@scottlogic.com>
2019-06-07 19:40:48 +0100

    Added visualisations

--- infrastructure/dead_letter_reporter/kibana.tf
+++ infrastructure/dead_letter_reporter/kibana.tf
@@ -36,15 +36,7 @@ module "dead_letter_index_snapshot" {
 
 module "dead_letter_visualisation" {
   source = "../kibana_saved_object"
-
-  // It is sometimes useful for the developers of the project to use a local version of the task
-  // execution project. This enables them to develop the task execution project and the nmap scanner
-  // (or other future tasks), at the same time, without requiring the task execution changes to be
-  // pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
-  // devs will have to comment in/out this line as and when they need
-  //  source = "../../../securityanalytics-analyticsplatform/infrastructure/kibana_saved_object"
   app_name = var.app_name
-
   aws_region       = var.aws_region
   ssm_source_stage = var.ssm_source_stage
   task_name        = "dead_letter"
