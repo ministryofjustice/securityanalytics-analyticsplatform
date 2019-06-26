02c0318c8e7e9577c89525f51d19a128885c9044
Matthew Amos <mamos@scottlogic.com>
2019-06-26 17:42:01 +0100

    WIP

--- queue_ingestor/queue_ingestor.py
+++ queue_ingestor/queue_ingestor.py
@@ -33,25 +33,57 @@ async def ingest(event, _):
         subject = body["Subject"]
         message = body["Message"]
 
+        # If there are message attributes present, then it is assumed that the message received
+        # is from the ResultsContext object. This contains a collection of document changes caused by
+        # one scan, and adds additional data in order to maintain a history and snapshot view of all those
+        # documents
         if "MessageAttributes" in body:
-            attrs = body["MessageAttributes"]
-            non_temporal_key = attrs["NonTemporalKey"]["Value"]
+            post_snap_and_history(body, endpoint, message, subject)
 
-            if "NonTemporalKey" in attrs and ("TemporalKey" in attrs or "ScanEndTime" in attrs):
-                await post_history_and_snapshot(attrs, endpoint, message, non_temporal_key, subject)
-            else:
-                post_to_es(endpoint, subject, message, non_temporal_key)
+        # Otherwise very simple logic is used to post the message body as a new event
         else:
             post_to_es(endpoint, subject, message)
 
 
-async def post_history_and_snapshot(attrs, endpoint, message, non_temporal_key, subject):
-    temporal_key = attrs["TemporalKey" if "TemporalKey" in attrs else "ScanEndTime"]["Value"]
-    # This post is the history, used in time series, note that key enables re-ingestion
-    post_to_es(endpoint, f"{subject}_history:write", message, f"{non_temporal_key}@{temporal_key}")
-    # This post is going to update the latest doc for this non temporal key
-    # i.e. this produces an index where we can access the latest version of each scan.
-    post_to_es(endpoint, f"{subject}_snapshot:write", message, non_temporal_key)
+def post_snap_and_history(body, endpoint, message, subject):
+    attrs = body["MessageAttributes"]
+
+    if not ("TemporalKey" in attrs or "ParentKey" in attrs):
+        raise ValueError("Analytics ingestor requires both the TemporalKey and ParentKey message attributes be present")
+
+    temporal_key = attrs["TemporalKey"]["Value"]
+    parent_key = attrs["ParentKey"]["Value"]
+
+    all_docs = loads(message)["__docs"]
+
+    for doc_type, docs in all_docs:
+        delete_old_snapshots(endpoint, subject, doc_type, parent_key)
+        for doc in docs:
+            non_temporal_key = doc["NonTemporalKey"]
+            content = doc["Data"]
+
+            # This post is the history, used in time series, note that key enables re-ingestion
+            post_to_es(endpoint, f"{subject}_history:write", content, f"{non_temporal_key}@{temporal_key}")
+            # This post is going to update the latest doc for this non temporal key
+            # i.e. this produces an index where we can access the latest version of each scan.
+            post_to_es(endpoint, f"{subject}_snapshot:write", content, non_temporal_key)
+
+
+def delete_old_snapshots(endpoint, subject, doc_type, parent_key):
+    es_url = f"https://{endpoint}/{subject}_snapshot:write/_doc/_delete_by_query?conflicts=proceed"
+    print(f"Deleting {doc_type} snapshots for {parent_key} using {es_url}")
+    delete_query = {
+      "query": {
+        "term": {
+            "__ParentKey": parent_key
+        }
+      }
+    }
+    r = requests.delete(es_url, auth=awsauth, data=dumps(delete_query), headers=HEADERS)
+    print(f"Delete completed {r.text}")
+    response_json = r.json()
+    if "error" in response_json.keys():
+        raise RuntimeError(dumps(response_json["error"]))
 
 
 def post_to_es(endpoint, subject, message, doc_id=None):
