2a9c45449a3ecdb15a068e0a6db9e15aac6341f1
Matthew Amos <mamos@scottlogic.com>
2019-07-02 09:07:26 +0100

    pointing to the wrong object

--- infrastructure/dead_letter_reporter/ssm_exports.tf
+++ infrastructure/dead_letter_reporter/ssm_exports.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "elastic_ingestion_queue_arn" {
   name        = "/${var.app_name}/${terraform.workspace}/analytics/kibana/dead_letter_index_pattern/id"
   description = "The object id of the viusalisation of dead letter queues"
   type        = "String"
-  value       = module.dead_letter_index_pattern.object_id
+  value       = module.dead_letter_visualisation.object_id
   overwrite   = "true"
 
   tags = {
